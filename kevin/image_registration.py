381f09c4f3fe7a0913998142a0d9c63c11992cd3
labuser <kyuhan14@gmail.com>
2022-10-27 13:10:39 -0400

    from lab computer

--- kevin/image_registration.py
+++ kevin/image_registration.py
@@ -1,7 +1,9 @@
 
 import numpy as np
 import time
-from PIL import Image
+from PIL import Image, ImageFile
+Image.MAX_IMAGE_PIXELS = None
+ImageFile.LOAD_TRUNCATED_IMAGES = True
 import cv2
 import os
 from matplotlib import pyplot as plt
@@ -62,110 +64,114 @@ num = int(len(img_files_path)/2) - 1 #idx = 16, or 17th image
 num_plus1 = num + 1 #idx = 17, or 18th image
 num_minus1 = num - 1 #idx = 15, or 16th image
 
-start = time()
-
-ref_img_path = img_files_path_complete[num]
-mov_img_path = img_files_path_complete[num_plus1]
-ref_img = np.array(Image.open(ref_img_path))
-mov_img = np.array(Image.open(mov_img_path))
-
-ref_img_g = rgb2gray(ref_img)
-mov_img_g = rgb2gray(mov_img)
-v, u = optical_flow_tvl1(ref_img_g, mov_img_g)
-nr, nc = ref_img_g.shape
-row_coords, col_coords = np.meshgrid(np.arange(nr), np.arange(nc),
-                                     indexing='ij')
-
-end = time()
-print("time it took to register: "+  str(end-start) + " seconds")
-
-start = time()
-
-mov_img_warp_ra =[]
-for i in range(3):
-    mov_img_warp = warp(mov_img[:,:,i], np.array([row_coords + v, col_coords + u]),mode='edge')
-    mov_img_warp_ra.append(mov_img_warp)
-
-r = np.array(mov_img_warp_ra[0]*255).astype('uint8')
-g = np.array(mov_img_warp_ra[1]*255).astype('uint8')
-b = np.array(mov_img_warp_ra[2]*255).astype('uint8')
-rgb = np.stack([r,g,b],axis=2)
-reg_img = Image.fromarray(rgb)
-reg_img.save(r'\\fatherserverdw\Kevin\imageregistration\registered_images\\' + str(img_files_path_1[num_plus1]) + '.png')
-
-end = time()
-print("time it took to register: "+  str(end-start) + " seconds")
-#%%
-# repeat again:
-start = time()
-
-ref_img_path = img_files_path_complete[num]
-mov_img_path = img_files_path_complete[num_minus1]
-ref_img = np.array(Image.open(ref_img_path))
-mov_img = np.array(Image.open(mov_img_path))
-
-ref_img_g = rgb2gray(ref_img)
-mov_img_g = rgb2gray(mov_img)
-v, u = optical_flow_tvl1(ref_img_g, mov_img_g)
-nr, nc = ref_img_g.shape
-row_coords, col_coords = np.meshgrid(np.arange(nr), np.arange(nc),
-                                     indexing='ij')
-end = time()
-print("time it took to register: "+  str(end-start) + " seconds")
-
-start = time()
-
-mov_img_warp_ra =[]
-for i in range(3):
-    mov_img_warp = warp(mov_img[:,:,i], np.array([row_coords + v, col_coords + u]),mode='edge')
-    mov_img_warp_ra.append(mov_img_warp)
-
-r = np.array(mov_img_warp_ra[0]*255).astype('uint8')
-g = np.array(mov_img_warp_ra[1]*255).astype('uint8')
-b = np.array(mov_img_warp_ra[2]*255).astype('uint8')
-rgb = np.stack([r,g,b],axis=2)
-reg_img = Image.fromarray(rgb)
-reg_img.save(r'\\fatherserverdw\Kevin\imageregistration\registered_images\\' + str(img_files_path_1[num_minus1]) + '.png')
-
-end = time()
-print("time it took to register: "+  str(end-start) + " seconds")
+# start = time()
+#
+# ref_img_path = img_files_path_complete[num]
+# mov_img_path = img_files_path_complete[num_plus1]
+# ref_img = np.array(Image.open(ref_img_path))
+# mov_img = np.array(Image.open(mov_img_path))
+#
+# ref_img_g = rgb2gray(ref_img)
+# mov_img_g = rgb2gray(mov_img)
+# v, u = optical_flow_tvl1(ref_img_g, mov_img_g)
+# nr, nc = ref_img_g.shape
+# row_coords, col_coords = np.meshgrid(np.arange(nr), np.arange(nc),
+#                                      indexing='ij')
+#
+# end = time()
+# print("time it took to register: "+  str(end-start) + " seconds")
+#
+# start = time()
+#
+# mov_img_warp_ra =[]
+# for i in range(3):
+#     mov_img_warp = warp(mov_img[:,:,i], np.array([row_coords + v, col_coords + u]),mode='edge')
+#     mov_img_warp_ra.append(mov_img_warp)
+#
+# r = np.array(mov_img_warp_ra[0]*255).astype('uint8')
+# g = np.array(mov_img_warp_ra[1]*255).astype('uint8')
+# b = np.array(mov_img_warp_ra[2]*255).astype('uint8')
+# rgb = np.stack([r,g,b],axis=2)
+# reg_img = Image.fromarray(rgb)
+# reg_img.save(r'\\fatherserverdw\Kevin\imageregistration\registered_images\\' + str(img_files_path_1[num_plus1]) + '.png')
+#
+# end = time()
+# print("time it took to register: "+  str(end-start) + " seconds")
+# #%%
+# # repeat again:
+# start = time()
+#
+# ref_img_path = img_files_path_complete[num]
+# mov_img_path = img_files_path_complete[num_minus1]
+# ref_img = np.array(Image.open(ref_img_path))
+# mov_img = np.array(Image.open(mov_img_path))
+#
+# ref_img_g = rgb2gray(ref_img)
+# mov_img_g = rgb2gray(mov_img)
+# v, u = optical_flow_tvl1(ref_img_g, mov_img_g)
+# nr, nc = ref_img_g.shape
+# row_coords, col_coords = np.meshgrid(np.arange(nr), np.arange(nc),
+#                                      indexing='ij')
+# end = time()
+# print("time it took to register: "+  str(end-start) + " seconds")
+#
+# start = time()
+#
+# mov_img_warp_ra =[]
+# for i in range(3):
+#     mov_img_warp = warp(mov_img[:,:,i], np.array([row_coords + v, col_coords + u]),mode='edge')
+#     mov_img_warp_ra.append(mov_img_warp)
+#
+# r = np.array(mov_img_warp_ra[0]*255).astype('uint8')
+# g = np.array(mov_img_warp_ra[1]*255).astype('uint8')
+# b = np.array(mov_img_warp_ra[2]*255).astype('uint8')
+# rgb = np.stack([r,g,b],axis=2)
+# reg_img = Image.fromarray(rgb)
+# reg_img.save(r'\\fatherserverdw\Kevin\imageregistration\registered_images\\' + str(img_files_path_1[num_minus1]) + '.png')
+#
+# end = time()
+# print("time it took to register: "+  str(end-start) + " seconds")
 #%%
 #two for loops to recursively create registered images:
-# from middle to end (higher index), idx of 17 to 33, or 18th image to 34th image:
-start = time()
-
-for idx in range(num_plus1,len(img_files_path_complete)): #idx = 17 to 34 (not including 34)
-    if idx == len(img_files_path_complete) - 1:
-        break
-    ref_img_path = os.path.join(r'\\fatherserverdw\Kevin\imageregistration\registered_images', img_files_path[idx])
-    mov_img_path = img_files_path_complete[idx+1]
-    ref_img = np.array(Image.open(ref_img_path))
-    mov_img = np.array(Image.open(mov_img_path))
-
-    ref_img_g = rgb2gray(ref_img)
-    mov_img_g = rgb2gray(mov_img)
-    v, u = optical_flow_tvl1(ref_img_g, mov_img_g)
-    nr, nc = ref_img_g.shape
-    row_coords, col_coords = np.meshgrid(np.arange(nr), np.arange(nc),
-                                         indexing='ij')
-    mov_img_warp_ra =[]
-    for i in range(3):
-        mov_img_warp = warp(mov_img[:,:,i], np.array([row_coords + v, col_coords + u]),mode='edge')
-        mov_img_warp_ra.append(mov_img_warp)
-    r = np.array(mov_img_warp_ra[0]*255).astype('uint8')
-    g = np.array(mov_img_warp_ra[1]*255).astype('uint8')
-    b = np.array(mov_img_warp_ra[2]*255).astype('uint8')
-    rgb = np.stack([r,g,b],axis=2)
-    reg_img = Image.fromarray(rgb)
-    reg_img.save(r'\\fatherserverdw\Kevin\imageregistration\registered_images\\' + str(img_files_path_1[idx+1]) + '.png')
-
-end = time()
-print("time it took to register: "+  str(end-start) + " seconds")
+# # from middle to end (higher index), idx of 17 to 33, or 18th image to 34th image:
+# start = time()
+#
+# for idx in range(num_plus1,len(img_files_path_complete)): #idx = 17 to 34 (not including 34)
+#     if idx == len(img_files_path_complete) - 1:
+#         break
+#     ref_img_path = os.path.join(r'\\fatherserverdw\Kevin\imageregistration\registered_images', img_files_path[idx])
+#     mov_img_path = img_files_path_complete[idx+1]
+#     ref_img = np.array(Image.open(ref_img_path))
+#     mov_img = np.array(Image.open(mov_img_path))
+#
+#     ref_img_g = rgb2gray(ref_img)
+#     mov_img_g = rgb2gray(mov_img)
+#     v, u = optical_flow_tvl1(ref_img_g, mov_img_g)
+#     nr, nc = ref_img_g.shape
+#     row_coords, col_coords = np.meshgrid(np.arange(nr), np.arange(nc),
+#                                          indexing='ij')
+#     mov_img_warp_ra =[]
+#     for i in range(3):
+#         mov_img_warp = warp(mov_img[:,:,i], np.array([row_coords + v, col_coords + u]),mode='edge')
+#         mov_img_warp_ra.append(mov_img_warp)
+#     r = np.array(mov_img_warp_ra[0]*255).astype('uint8')
+#     g = np.array(mov_img_warp_ra[1]*255).astype('uint8')
+#     b = np.array(mov_img_warp_ra[2]*255).astype('uint8')
+#     rgb = np.stack([r,g,b],axis=2)
+#     reg_img = Image.fromarray(rgb)
+#     reg_img.save(r'\\fatherserverdw\Kevin\imageregistration\registered_images\\' + str(img_files_path_1[idx+1]) + '.png')
+#
+# end = time()
+# print("time it took to register: "+  str(end-start) + " seconds")
 #%%
+
 # from middle to beginning (lower index), idx of 15 to 0, or 16th image to 1st image:
 start = time()
 
+dst = r'\\fatherserverdw\Kevin\imageregistration\registered_images'
 for idx in range(num_minus1,-1,-1): #idx = 15 to -1 (not including -1)
+    dstfn = os.path.join(dst,img_files_path[idx-1])
+    if os.path.exists(dstfn): continue
     if idx == 0:
         break
     ref_img_path = os.path.join(r'\\fatherserverdw\Kevin\imageregistration\registered_images', img_files_path[idx])
@@ -188,7 +194,7 @@ for idx in range(num_minus1,-1,-1): #idx = 15 to -1 (not including -1)
     b = np.array(mov_img_warp_ra[2]*255).astype('uint8')
     rgb = np.stack([r,g,b],axis=2)
     reg_img = Image.fromarray(rgb)
-    reg_img.save(r'\\fatherserverdw\Kevin\imageregistration\registered_images\\' + str(img_files_path[idx-1]) + '.png')
+    reg_img.save(dstfn)
 
 end = time()
 print("time it took to register: "+  str(end-start) + " seconds")
